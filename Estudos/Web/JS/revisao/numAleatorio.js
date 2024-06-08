8a0a637119e5683e77d333de5d13838e90d0046c
tbaiense <thiago.baiense@aluno.senai.br>
2024-06-08 17:46:16 -0300

    adicionado js

--- Estudos/Web/JS/revisao/numAleatorio.js
+++ Estudos/Web/JS/revisao/numAleatorio.js
@@ -1,16 +1,18 @@
 "use strict";
-
+// Gera um número entre o intervalo definido (1-99)
 function rand (min = 0, max= 100) {
     max = Math.floor(max);
     min = Math.ceil(min);
     return Math.floor(Math.random() * (max-min) + min);
 }
 
-let array = Array(1000)
+let array = Array(100)
 for (let i=0; i<array.length; i++) {
     array[i] = rand(0.2, array.length*10);
 }
-
-let p = document.querySelector("#parag");
-p.textContent = array.join(' ');
-
+let menor=array[0];
+array.forEach((elem) => {
+    if (menor > elem) 
+        menor = elem;
+});
+console.log(menor);
