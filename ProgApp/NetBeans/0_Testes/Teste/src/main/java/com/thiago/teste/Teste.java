762b0e1dee47b5940d852783aaf06a548690d20a
t.baiense <thiago.baiense@aluno.senai.br>
2024-08-28 20:18:15 -0300

    exercicio 12 java

--- ProgApp/NetBeans/0_Testes/Teste/src/main/java/com/thiago/teste/Teste.java
+++ ProgApp/NetBeans/0_Testes/Teste/src/main/java/com/thiago/teste/Teste.java
@@ -1,16 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.thiago.teste;
+import java.util.Scanner;
 
-/**
- *
- * @author t.baiense
- */
 public class Teste {
-
+    private static String[] nomes = new String[10]; // Nomes inseridos pelo usuário
+    private static int qntNomesReg; // Contabiliza quantos nomes foram inseridos
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Escreva os nomes a seguir:");
+        for (int n = 0; n < 10; ++n)
+            nomes[n] = receberNome();
+        exibirNomes(nomes);
+    }
+    
+    private static String receberNome() {
+        System.out.print(++qntNomesReg + ") >>> ");
+        Scanner stdin = new Scanner(System.in);
+        return stdin.nextLine();
+    }
+    
+    private static void exibirNomes(String[] nomes) {
+        System.out.printf("\n# Nomes registrados (%s):\n", qntNomesReg);
+        for (String nome : nomes) // Percorre o vetor `nomes`, colocando cada String em `nome`
+            System.out.println(nome);
     }
 }
---
+++ ProgApp/NetBeans/PROJETO_12_THIAGO/src/main/java/com/thiago/projeto_12_thiago/PROJETO_12_THIAGO.java
@@ -0,0 +1,61 @@
+package com.thiago.projeto_12_thiago;
+import java.util.Scanner;
+
+/**
+ * Essa classe implementa um pequeno jogo de adivinhação de filmes,
+ * onde o usuário adivinha o filme onde outro jogador especificou.
+ */
+public class PROJETO_12_THIAGO {
+
+    public static void main(String[] args) {
+        Scanner stdin = new Scanner(System.in);
+        String jogadorUm, jogadorDois; // Nome dos jogadores
+        String filme; // Nome do filme, inserido pelo jogador um
+        short qntPistas = 5;
+        short tentativas = 5; // Número de tentativas para o jogador 2
+        
+        System.out.print("Jogador 1 - Insira seu nome: ");
+        jogadorUm = stdin.nextLine();
+        System.out.print(jogadorUm + " - Especifique o nome do filme: ");
+        filme = stdin.nextLine().trim();
+        
+        // Dicas
+
+        String[] pistas = new String[qntPistas]; // Armazena as pistas inseridas pelo jogador um
+        for (int pn = 0; pn < qntPistas; ++pn) {
+            System.out.printf("Pista n.%d : ", pn+1);
+            pistas[pn] = stdin.nextLine();
+        }
+        
+        System.out.println("------------------------------");
+        
+        //Cadastro jogador dois
+        System.out.print("Jogador 2 - Insira seu nome: ");
+        jogadorDois = stdin.nextLine();
+        
+        String escolha;
+        System.out.printf(jogadorDois + " - \nPista: %s\n Advinhe o nome do filme: ", pistas[qntPistas - 1]);
+        --qntPistas; 
+        
+        // Escolhas
+        do {
+            escolha = stdin.nextLine().trim();
+            --tentativas;
+            if (filme.equalsIgnoreCase(escolha)) {
+                System.out.println("ACERTOU!!");
+                break;
+            } else {
+                System.out.println(jogadorDois + ", você errou!");
+                if (tentativas > 0) { // Se houver tentativas restantes
+                    if (qntPistas > 0) {// Se houver pistas restantes
+                        System.out.println(qntPistas + " pista restante: " + pistas[qntPistas - 1]);
+                        --qntPistas;
+                    }
+                    System.out.print("Tente novamente: ");
+                }
+            }
+        } while (tentativas > 0);
+            
+    }
+    
+}
