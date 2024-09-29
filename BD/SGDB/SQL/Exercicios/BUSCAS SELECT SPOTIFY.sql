b5ecd046850fd5f9c14b97782eae32e9ae00e5e2
tbaiense <thiago.baiense@aluno.senai.br>
2024-09-29 00:58:01 -0300

    atualiza exercicio de bd: finalizado

--- BD/SGDB/SQL/Exercicios/BUSCAS SELECT SPOTIFY.sql
+++ BD/SGDB/SQL/Exercicios/BUSCAS SELECT SPOTIFY.sql
@@ -8,14 +8,14 @@ SELECT COUNT(posicao) FROM spotifytop;
 
 -- 3 Contagem de quantas músicas estiveram no top 1
 SELECT COUNT(maiorPosicao) AS QTD_MUSICAS FROM spotifytop
-WHERE maiorPosicao = 1; 
+WHERE maiorPosicao = 1;
 
 -- 4 Qual a música que ficou por mais vezes no top 1
 SELECT nomeArtista, nomeMusica, maiorPosicao, vezesMaiorPosicao FROM spotifytop
 WHERE vezesMaiorPosicao = (
 	SELECT MAX(vezesMaiorPosicao) FROM spotifytop
     WHERE maiorPosicao = 1);
-    
+
 -- 5 Quantos artistas diferentes há
 SELECT COUNT(DISTINCT nomeArtista) AS QTD_ARTISTAS FROM spotifytop;
 
@@ -28,14 +28,19 @@ WHERE totalStreams = (SELECT MAX(totalStreams) FROM spotifytop);
 SELECT nomeMusica, nomeArtista, picoDeStreams FROM spotifytop
 WHERE picoDeStreams = (SELECT MAX(picoDeStreams) FROM spotifytop);
 
--- X  8 Qual artista ficou mais vezes no top 1
-SELECT nomeArtista, SUM(vezesMaiorPosicao) FROM spotifytop
-GROUP BY nomeArtista;
+-- 8 Qual artista ficou mais vezes no top 1
+SELECT nomeArtista, SUM(vezesMaiorPosicao) AS TOP1_TOTAL
+FROM spotifytop
+WHERE maiorPosicao = 1
+GROUP BY nomeArtista ORDER BY TOP1_TOTAL DESC
+LIMIT 1;
 
-
--- X9 Qual artistas possui mais registros
-SELECT nomeArtista, COUNT(nomeMusica) as QTD_VEZES FROM spotifytop
-GROUP BY nomeArtista order by QTD_VEZES DESC;
+-- 9 Qual artistas possui mais registros
+SELECT nomeArtista, COUNT(nomeMusica) AS QTD_MUSICAS
+FROM spotifytop
+GROUP BY nomeArtista
+ORDER BY QTD_MUSICAS DESC
+LIMIT 1;
 
 -- 10 Quantos artistas possuem nome iniciando com a letra “H”
 SELECT COUNT(nomeArtista) AS QTD_ARTISTAS FROM spotifytop
@@ -68,3 +73,52 @@ WHERE nomeMusica LIKE '%girl%';
 SELECT SUM(totalStreams) AS TOTAL_STREAMS FROM spotifytop
 WHERE nomeArtista = "Post Malone";
 
+-- 17 Quais são os 5 artistas com mais registros, de forma decrescente
+SELECT nomeArtista, COUNT(nomeMusica) AS QTD_MUSICAS
+FROM spotifytop
+GROUP BY nomeArtista
+ORDER BY QTD_MUSICAS
+LIMIT 5;
+
+-- 18 Qual o total de streams das 10 músicas mais com mais streams
+SELECT posicao, nomeMusica, totalStreams
+FROM spotifytop
+WHERE totalStreams IS NOT NULL
+ORDER BY totalStreams DESC
+LIMIT 10;
+
+SELECT SUM(totalStreams) AS TOTAL_TOP10
+FROM spotifytop
+WHERE posicao BETWEEN 1 AND 10;
+
+-- 19 Quais as músicas que já estiveram no top 1 e estiveram entre 40 e 60 vezes a maior posição
+SELECT nomeMusica, maiorPosicao, vezesMaiorPosicao
+FROM spotifytop
+WHERE maiorPosicao = 1 AND vezesMaiorPosicao BETWEEN 40 AND 60
+ORDER BY vezesMaiorPosicao DESC;
+
+-- 20 Quais músicas tiveram menos de 500 mil streams e ficaram entre o top 5 e 10
+SELECT nomeMusica, totalStreams, maiorPosicao
+FROM spotifytop
+WHERE totalStreams < 500000 AND maiorPosicao BETWEEN 5 AND 10
+ORDER BY totalStreams DESC;
+
+-- 21 Qual a música com o mínimo de streams que atingiu a 1° posição no top 10
+SELECT nomeMusica, totalStreams, maiorPosicao
+FROM spotifytop
+WHERE totalStreams = (SELECT MIN(totalStreams) FROM spotifytop WHERE maiorPosicao = 1);
+
+-- 22 Qual a música com o mínimo de streams da artista Taylor Swift
+SELECT nomeMusica, nomeArtista, totalStreams
+FROM spotifytop
+WHERE totalStreams = (
+	SELECT MIN(totalStreams)
+    FROM spotifytop
+    WHERE nomeArtista = "Taylor Swift");
+
+SELECT maiorPosicao, SUM(totalStreams), COUNT(posicao)
+FROM spotifytop
+WHERE totalStreams < 100000
+GROUP BY maiorPosicao
+HAVING maiorPosicao BETWEEN MIN(posicao) AND MAX(posicao)
+ORDER BY maiorPosicao ASC;
